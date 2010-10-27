b8cb3fc492327cf96da981e5be1ae5f3bb5a5564
Sascha Daniels <sd@alternative-solution.de>
2010-10-28 01:37:29 +0200

    Keyboard layout. Geht immer noch nicht :-(

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-#PACKAGES="shorewall"
 BOOT_APPEND="live-config.locales=de live-config.keyb=de"
-
-
 
 lb clean 
 
-lb config  --memtest none --username pbx --bootappend-live "$BOOT_APPEND" && lb build
-
+lb config -p standard --memtest none --username pbx --bootappend-live "$BOOT_APPEND" && lb build
 
